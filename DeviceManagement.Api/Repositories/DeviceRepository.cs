2deb31609656e9758d1308008393afbebe89af2d
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-09 18:18:52 +0300

    feat(api): implement Device and User repositories

--- DeviceManagement.Api/Repositories/DeviceRepository.cs
+++ DeviceManagement.Api/Repositories/DeviceRepository.cs
@@ -1,6 +1,53 @@
+using Dapper;
+using DeviceManagement.Api.Data;
+using DeviceManagement.Api.Models;
+using DeviceManagement.Api.DTOs;
+using DeviceManagement.Api.Repositories.Interfaces;
+
 namespace DeviceManagement.Api.Repositories;
 
-public class DeviceRepository
+public class DeviceRepository : IDeviceRepository
 {
-    
+    public async Task<List<Device>> GetAllDevicesAsync()
+    {
+        using var connection = DatabaseConfig.GetDatabaseConnection();
+
+        var query = "SELECT * FROM Devices";
+        var devices = (await connection.QueryAsync<Device>(query)).ToList();
+
+        return devices;
+    }
+
+    public async Task<Device> GetDeviceByIdAsync(int id)
+    {
+        using var connection = DatabaseConfig.GetDatabaseConnection();
+
+        var query = "SELECT * FROM Devices WHERE id = @id";
+        var device = await connection.QueryFirstOrDefaultAsync<Device>(query, new { id });
+
+        return device;
+    }
+
+    public async Task AddDeviceAsync(DeviceDTO device)
+    {
+        using var connection = DatabaseConfig.GetDatabaseConnection();
+        var query =
+            "INSERT INTO Devices (name, d_type, os, os_version, processor, ram, description, current_user_id, manufacturer) VALUES (@Name, @Type, @OperatingSystem, @OsVersion, @Processor, @Ram, @Description, @UserId, @Manufacturer)";
+        await connection.ExecuteAsync(query, device);
+    }
+
+    public async Task UpdateDeviceAsync(DeviceDTO device)
+    {
+        using var connection = DatabaseConfig.GetDatabaseConnection();
+        var query =
+            "UPDATE Devices SET name = @Name, d_type = @Type, os = @OperatingSystem, os_version = @OsVersion, processor = @Processor, ram = @Ram, description = @Description, current_user_id = @UserId, manufacturer = @Manufacturer WHERE id = @Id";
+        await connection.ExecuteAsync(query, device);
+    }
+
+    public async Task DeleteDeviceAsync(int id)
+    {
+        using var connection = DatabaseConfig.GetDatabaseConnection();
+        var query = "DELETE FROM Devices WHERE id = @id";
+        await connection.ExecuteAsync(query, new { id });
+    }
 }
--- DeviceManagement.Api/Repositories/UserRepository.cs
+++ DeviceManagement.Api/Repositories/UserRepository.cs
@@ -1,6 +1,49 @@
+using Dapper;
+using DeviceManagement.Api.Data;
+using DeviceManagement.Api.DTOs;
+using DeviceManagement.Api.Models;
+using DeviceManagement.Api.Repositories.Interfaces;
+
 namespace DeviceManagement.Api.Repositories;
 
-public class UserRepository
+public class UserRepository : IUserRepository
 {
-    
+    public async Task<List<User>> GetAllUsersAsync()
+    {
+        using var connection = DatabaseConfig.GetDatabaseConnection();
+        var query = "SELECT * FROM Users";
+        var users = (await connection.QueryAsync<User>(query)).ToList();
+
+        return users;
+    }
+
+    public async Task<User> GetUserByIdAsync(int id)
+    {
+        using var connection = DatabaseConfig.GetDatabaseConnection();
+        var query = "SELECT * FROM Users WHERE id = @id";
+        var user = await connection.QueryFirstOrDefaultAsync<User>(query, new { id });
+
+        return user;
+    }
+
+    public async Task AddUserAsync(UserDTO user)
+    {
+        using var connection = DatabaseConfig.GetDatabaseConnection();
+        var query = "INSERT INTO Users (name, role, location) VALUES (@Name, @Role, @Location)";
+        await connection.ExecuteAsync(query, user);
+    }
+
+    public async Task UpdateUserAsync(UserDTO user)
+    {
+        using var connection = DatabaseConfig.GetDatabaseConnection();
+        var query = "UPDATE Users SET name = @Name, role = @Role, location = @Location WHERE id = @Id";
+        await connection.ExecuteAsync(query, user);
+    }
+
+    public async Task DeleteUserAsync(int id)
+    {
+        using var connection = DatabaseConfig.GetDatabaseConnection();
+        var query = "DELETE FROM Users WHERE id = @id";
+        await connection.ExecuteAsync(query, new { id });
+    }
 }
