2c400fa2ad1b120f65e8842dabda1a173d095744
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-09 18:18:51 +0300

    feat(api): configure database connection and add Dapper dependency

--- DeviceManagement.Api/Data/DatabaseConfig.cs
+++ DeviceManagement.Api/Data/DatabaseConfig.cs
@@ -1,6 +1,15 @@
+using Microsoft.Data.SqlClient;
+
 namespace DeviceManagement.Api.Data;
 
 public class DatabaseConfig
 {
-    
+    public static readonly string ConnectionString = 
+        @"Server=localhost,1433;Database=DeviceManagementSystem;User Id=sa;Password=Parola@12345;TrustServerCertificate=True;";
+
+    public static SqlConnection GetDatabaseConnection()
+    {
+        return new SqlConnection(ConnectionString);
+    }
+
 }
