889932091842e35614b02f44c39415a60802b23a
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-09 18:18:51 +0300

    feat(api): add repository interfaces

--- DeviceManagement.Api/Repositories/Interfaces/IDeviceRepository.cs
+++ DeviceManagement.Api/Repositories/Interfaces/IDeviceRepository.cs
@@ -1,6 +1,13 @@
+using DeviceManagement.Api.DTOs;
+using DeviceManagement.Api.Models;
+
 namespace DeviceManagement.Api.Repositories.Interfaces;
 
-public class IDeviceRepository
+public interface IDeviceRepository
 {
-    
+    Task<List<Device>> GetAllDevicesAsync();
+    Task<Device> GetDeviceByIdAsync(int id);
+    Task AddDeviceAsync(DeviceDTO device);
+    Task UpdateDeviceAsync(DeviceDTO device);
+    Task DeleteDeviceAsync(int id);
 }
--- DeviceManagement.Api/Repositories/Interfaces/IUserRepository.cs
+++ DeviceManagement.Api/Repositories/Interfaces/IUserRepository.cs
@@ -1,6 +1,16 @@
+using DeviceManagement.Api.DTOs;
+using DeviceManagement.Api.Models;
+
 namespace DeviceManagement.Api.Repositories.Interfaces;
 
-public class IUserRepository
+public interface IUserRepository
 {
+    Task<List<User>>GetAllUsersAsync();
+    Task<User> GetUserByIdAsync(int id);
+    Task AddUserAsync(UserDTO user);
+    Task UpdateUserAsync(UserDTO user);
+    Task DeleteUserAsync(int id);
+ 
+    
     
 }
