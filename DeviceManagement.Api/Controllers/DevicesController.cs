aa2926f1560b9dc0a502d3bf56d1cac6de15decd
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-10 15:45:14 +0300

    feat(api): add Devices and Users controllers with CRUD endpoints

--- DeviceManagement.Api/Controllers/DevicesController.cs
+++ DeviceManagement.Api/Controllers/DevicesController.cs
@@ -1,6 +1,63 @@
+using DeviceManagement.Api.DTOs;
+using DeviceManagement.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DeviceManagement.Api.Controllers;
 
-public class DevicesController
+[ApiController]
+[Route("api/[controller]")]
+public class DevicesController(IDeviceService deviceService) : ControllerBase
 {
-    
+    private readonly IDeviceService _deviceService = deviceService;
+
+    [HttpGet]
+    public async Task<IActionResult> GetDevices()
+    {
+        var devices = await _deviceService.GetAllDevicesAsync();
+        return Ok(devices);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetDeviceById(int id)
+    {
+        var device = await _deviceService.GetDeviceByIdAsync(id);
+        if (device == null) return NotFound();
+        return Ok(device);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddDevice([FromBody] DeviceDTO device)
+    {
+        var id = await _deviceService.AddDeviceAsync(device);
+        var created = await _deviceService.GetDeviceByIdAsync(id);
+        return CreatedAtAction(nameof(GetDeviceById), new { id }, created);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateDevice(int id, DeviceDTO device)
+    {
+        try
+        {
+            await _deviceService.UpdateDeviceAsync(id, device);
+            return NoContent();
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteDevice(int id)
+    {
+        try
+        {
+            await _deviceService.DeleteDeviceAsync(id);
+            return NoContent();
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
 }
--- DeviceManagement.Api/Controllers/UsersController.cs
+++ DeviceManagement.Api/Controllers/UsersController.cs
@@ -1,6 +1,63 @@
+using DeviceManagement.Api.DTOs;
+using DeviceManagement.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DeviceManagement.Api.Controllers;
 
-public class UsersController
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(IUserService userService) : ControllerBase
 {
-    
+    private readonly IUserService _userService = userService;
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+        var users = await _userService.GetAllUsersAsync();
+        return Ok(users);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserById(int id)
+    {
+        var user = await _userService.GetUserByIdAsync(id);
+        if (user == null) return NotFound();
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddUser([FromBody] UserDTO user)
+    {
+        var id = await _userService.AddUserAsync(user);
+        var created = await _userService.GetUserByIdAsync(id);
+        return CreatedAtAction(nameof(GetUserById), new { id }, created);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(int id, UserDTO user)
+    {
+        try
+        {
+            await _userService.UpdateUserAsync(id, user);
+            return NoContent();
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        try
+        {
+            await _userService.DeleteUserAsync(id);
+            return NoContent();
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
 }
