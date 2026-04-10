f9375f773c8b947d0fab40ea7d66d45c3c6df4a6
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-10 15:44:48 +0300

    feat(api): add Device and User service interfaces

--- DeviceManagement.Api/Services/Interfaces/IDeviceService.cs
+++ DeviceManagement.Api/Services/Interfaces/IDeviceService.cs
@@ -1,6 +1,13 @@
+using DeviceManagement.Api.DTOs;
+using DeviceManagement.Api.Models;
+
 namespace DeviceManagement.Api.Services.Interfaces;
 
-public class IDeviceService
+public interface IDeviceService
 {
-    
+    Task<List<Device>> GetAllDevicesAsync();
+    Task<Device?> GetDeviceByIdAsync(int id);
+    Task<int> AddDeviceAsync(DeviceDTO device);
+    Task UpdateDeviceAsync(int id,DeviceDTO device);
+    Task DeleteDeviceAsync(int id);
 }
--- DeviceManagement.Api/Services/Interfaces/IUserService.cs
+++ DeviceManagement.Api/Services/Interfaces/IUserService.cs
@@ -1,6 +1,15 @@
+using DeviceManagement.Api.DTOs;
+using DeviceManagement.Api.Models;
+
 namespace DeviceManagement.Api.Services.Interfaces;
 
-public class IUserService
+public interface IUserService
 {
+    Task<List<User>>GetAllUsersAsync();
+    Task<User?> GetUserByIdAsync(int id);
+    Task<int> AddUserAsync(UserDTO user);
+    Task UpdateUserAsync(int id, UserDTO user);
+    Task DeleteUserAsync(int id);
+
     
 }
