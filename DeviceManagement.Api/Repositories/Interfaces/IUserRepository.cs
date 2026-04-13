6d2ad8f8861484a7571def1545b3a87eb0d16477
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-13 13:36:51 +0300

    feat(api): add email lookup and auth user insert to repository

--- DeviceManagement.Api/Repositories/Interfaces/IUserRepository.cs
+++ DeviceManagement.Api/Repositories/Interfaces/IUserRepository.cs
@@ -7,9 +7,12 @@ public interface IUserRepository
 {
     Task<List<User>>GetAllUsersAsync();
     Task<User?> GetUserByIdAsync(int id);
+    Task<User?> GetUserByEmailAsync(string email);
     Task<int> AddUserAsync(UserDTO user);
     Task<int> UpdateUserAsync(int id, UserDTO user);
     Task<int> DeleteUserAsync(int id);
+    
+    Task<int> AddAuthUserAsync(User user);
  
     
     
--- DeviceManagement.Api/Repositories/UserRepository.cs
+++ DeviceManagement.Api/Repositories/UserRepository.cs
@@ -1,5 +1,4 @@
 using Dapper;
-using DeviceManagement.Api.Data;
 using DeviceManagement.Api.Data.Interfaces;
 using DeviceManagement.Api.DTOs;
 using DeviceManagement.Api.Models;
@@ -24,11 +23,21 @@ public class UserRepository(IDbConnectionFactory dbConfig) : IUserRepository
     public async Task<User?> GetUserByIdAsync(int id)
     {
         using var connection = _dbConfig.CreateConnection();
-        var query = "SELECT * FROM Users WHERE id = @id";
+        var query = "SELECT id, name, role, location, email, password_hash AS PasswordHash FROM Users WHERE id = @id";
         var user = await connection.QueryFirstOrDefaultAsync<User>(query, new { id });
 
         return user;
     }
+    
+    public async Task<User?> GetUserByEmailAsync(string email)
+    {
+        using var connection = _dbConfig.CreateConnection();
+        var query = "SELECT id, name,role,location,email,password_hash AS PasswordHash FROM Users WHERE email = @email";
+        var user = await connection.QueryFirstOrDefaultAsync<User>(query, new { email });
+
+        return user;
+    }
+
 
     public async Task<int> AddUserAsync(UserDTO user)
     {
@@ -59,5 +68,14 @@ public class UserRepository(IDbConnectionFactory dbConfig) : IUserRepository
         var query = "DELETE FROM Users WHERE id = @id";
         var rowsAffected = await connection.ExecuteAsync(query, new { id });
         return rowsAffected;
+    }
+
+    public async Task<int> AddAuthUserAsync(User user)
+    {
+        using var connection = _dbConfig.CreateConnection();
+        var query =
+            "INSERT INTO Users (name, role, location, email, password_hash) VALUES (@Name, @Role, @Location, @Email, @PasswordHash); Select SCOPE_IDENTITY();";
+        var newId = await connection.ExecuteScalarAsync<int>(query, user);
+        return newId;
     }
 }
