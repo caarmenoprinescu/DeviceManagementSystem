65efbf820c6ecf82638ced8561416c16bd2ddc0a
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-13 13:41:03 +0300

    feat(client): add auth routes and configure interceptor

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { DeviceList } from './features/devices/device-list/device-list';
 import { DeviceForm } from './features/devices/device-form/device-form';
 import { DeviceDetail } from './features/devices/device-detail/device-detail';
+import { Register } from './features/auth/register/register';
+import { Login } from './features/auth/login/login';
 export const routes: Routes = [
   { path: '', redirectTo: 'devices', pathMatch: 'full' },
   { path: 'devices', component: DeviceList },
   { path: 'devices/new', component: DeviceForm },
   { path: 'devices/:id', component: DeviceDetail },
-
   { path: 'devices/:id/edit', component: DeviceForm },
+    { path: 'auth/login', component: Login},
+  { path: 'auth/register', component: Register },
 ];
