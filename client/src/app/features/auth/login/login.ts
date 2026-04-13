20069bb3b7211f9f30b4f3c80f1204e8a38a4ec7
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-13 13:41:03 +0300

    feat(client): add login and register components

---
+++ client/src/app/features/auth/login/login.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { AuthService } from '../../../core/services/auth.service';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    MatIcon,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  loginData = { email: '', password: '' };
+  errorMessage = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  login(): void {
+    this.authService.login(this.loginData).subscribe({
+      next: () => this.router.navigate(['/devices']),
+      error: (err) => (this.errorMessage = err.error),
+    });
+  }
+}
