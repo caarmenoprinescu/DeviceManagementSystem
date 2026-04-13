2b538b925e08fa65c1c2e485eada500c7a3c4d79
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-13 13:33:43 +0300

    feat(api): add email and password hash fields to User model

--- DeviceManagement.Api/Models/User.cs
+++ DeviceManagement.Api/Models/User.cs
@@ -6,4 +6,6 @@ public class User
     public string Name { get; set; }
     public string Role { get; set; }
     public string Location { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
 }
