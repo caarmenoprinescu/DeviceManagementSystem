6cb4f813f6548e21f6b05f3bfc1a7edebc1c9689
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-13 13:37:13 +0300

    feat(api): add auth controller with register and login endpoints

--- DeviceManagement.Api/Controllers/AuthController.cs
+++ DeviceManagement.Api/Controllers/AuthController.cs
@@ -1,6 +1,40 @@
+using DeviceManagement.Api.DTOs;
+using DeviceManagement.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DeviceManagement.Api.Controllers;
 
-public class AuthController
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController(IAuthService authService) : ControllerBase
 {
-    
+    private readonly IAuthService _authService = authService;
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterDTO register)
+    {
+        try
+        {
+            var registered = await _authService.Register(register);
+            return Created("", registered);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDTO login)
+    {
+        try
+        {
+            var loggedIn = await _authService.Login(login);
+            return Ok(loggedIn);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
 }
