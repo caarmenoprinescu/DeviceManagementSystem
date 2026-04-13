b03191c54554b188f8493e0aa2261968475d4dd0
caarmenoprinescu <oprinescucarmen@gmail.com>
2026-04-13 22:35:31 +0300

    feat(client): add user routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,12 +4,19 @@ import { DeviceForm } from './features/devices/device-form/device-form';
 import { DeviceDetail } from './features/devices/device-detail/device-detail';
 import { Register } from './features/auth/register/register';
 import { Login } from './features/auth/login/login';
+import { UserList } from './features/users/user-list/user-list';
+import { UserDetail } from './features/users/user-detail/user-detail';
+import { UserForm } from './features/users/user-form/user-form';
 export const routes: Routes = [
   { path: '', redirectTo: 'devices', pathMatch: 'full' },
   { path: 'devices', component: DeviceList },
   { path: 'devices/new', component: DeviceForm },
   { path: 'devices/:id', component: DeviceDetail },
   { path: 'devices/:id/edit', component: DeviceForm },
-    { path: 'auth/login', component: Login},
+  { path: 'users', component: UserList },
+{ path: 'users/new', component: UserForm },
+{ path: 'users/:id/edit', component: UserForm },
+{ path: 'users/:id', component: UserDetail },
+  { path: 'auth/login', component: Login },
   { path: 'auth/register', component: Register },
 ];
